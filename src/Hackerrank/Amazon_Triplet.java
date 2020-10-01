b65e452ea6050707da0d56efc5aa55195f40ae20
Raiden8051 <www.sidharth.56@gmail.com>
2020-10-02 00:20:53 +0530

    update 02 Oct 2020

---
+++ src/Hackerrank/Amazon_Triplet.java
@@ -0,0 +1,32 @@
+package Hackerrank;
+import java.util.*;
+public class Amazon_Triplet {
+
+        static List<String> li = new ArrayList<>();
+        public static void main(String args[] ) throws Exception {
+
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+
+            sc.nextLine();
+
+            String[] s = sc.nextLine().split(" ");
+
+            String[] d = new String[3];
+
+            getData(s,d,0,n-1,0,3);
+
+            System.out.println(li.size());
+
+        }
+        static void getData(String[] arr,String[] d,int s, int e, int index, int triplet){
+            if(index==triplet){
+                if(Integer.parseInt(d[0])<Integer.parseInt(d[1]) && Integer.parseInt(d[1])>Integer.parseInt(d[2]))
+                    li.add(d[0]+""+d[1]+""+d[2]);
+            }
+            for(int i=s;i<=e && e-(i+1) >=triplet-index;i++){
+                d[index] = arr[i];
+                getData(arr,d,i+1,e,index+1,triplet);
+            }
+        }
+    }
--- src/Hackerrank/MorganAndAString.java
+++ src/Hackerrank/MorganAndAString.java
@@ -1,5 +1,59 @@
 package Hackerrank;
+import java.io.*;
+import java.util.*;
 
 public class MorganAndAString {
+
+
+    // Complete the morganAndString function below.
+    static String morganAndString(String a, String b) {
+
+        int a_ctr = 0, b_ctr = 0, i = 0;
+        String ach, bch;
+        StringBuilder result = new StringBuilder();
+        StringBuilder asb = new StringBuilder(a);
+        StringBuilder bsb = new StringBuilder(b);
+        while(i++<a.length()+b.length()){
+            try {
+                ach = Character.toString(asb.charAt(0));
+                bch = Character.toString(bsb.charAt(0));
+                if (ach.compareTo(bch) < 0 || ach.compareTo(bch) == 0) {
+                    result.append(ach);
+                    asb.deleteCharAt(0);
+                } else {
+                    result.append(bch);
+                    bsb.deleteCharAt(0);
+                }
+            }
+            catch (StringIndexOutOfBoundsException ex) {
+                if(asb.length()==0)
+                    result.append(bsb);
+                else
+                    result.append(asb);
+                break;
+            }
+        }
+        return result.toString();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String a = scanner.nextLine();
+
+            String b = scanner.nextLine();
+
+            String result = morganAndString(a, b);
+
+            System.out.println(result);
+        }
+
+        scanner.close();
+    }
 
 }
